8a240b5bfe5f7527b3592e5f47c948d32c667a70
LikZeneli <likzeneli123@gmail.com>
2024-01-24 08:49:32 -0800

    FirstCommit
    MainMethod Added

---
+++ src/Metoda3.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Metoda3{
+
+
+    public static void main(String[] args) {
+        Autobiografia("Fadil","Zeneli",42,1.84,"Vranjevc","Sportist");
+    }
+
+    public static void Autobiografia(String emri, String mbiemri, int mosha, double gjatesia, String venbanimi, String profesioni){
+        System.out.println("emri im esht :"+emri);
+        System.out.println("mbiemri im esht :"+mbiemri);
+        System.out.println("mosha ime esht :"+mosha);
+        System.out.println("gjatesia ime esht :"+gjatesia);
+        System.out.println("venbanimi im esht :"+venbanimi);
+        System.out.println("profesioni im esht :"+profesioni);
+
+    }
+
+}
+
+
+
+
+
+
