4d3fbc2f5f3a80e24bd74805361b457a3c293f82
LikZeneli <likzeneli123@gmail.com>
2024-01-26 10:31:12 -0800

    Parametrat

---
+++ src/Parametrat/Parametrat.java
@@ -0,0 +1,13 @@
+package src.Parametrat;
+
+public class Parametrat {
+    public static void main(String[] args) {
+        System.out.println(shuma(5335,6565));
+
+
+    }
+    public static int shuma(int x, int y){
+        return x+y;
+
+    }
+}
