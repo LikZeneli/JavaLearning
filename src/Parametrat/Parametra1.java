ea3a771c8212ae68e4f848452bf0cb82c8c45f32
LikZeneli <likzeneli123@gmail.com>
2024-01-29 08:19:00 -0800

    zbritja

---
+++ src/Parametrat/Parametra1.java
@@ -0,0 +1,13 @@
+package src.Parametrat;
+
+public class Parametra1 {
+
+    public static void main(String[] args) {
+        System.out.println(zbritja(3678,2977));
+
+    }
+
+    public static int zbritja(int a, int b){
+        return a-b;
+    }
+}
