f1cf14b88c065d128747ae2bfe57ad0bc6bc2019
LikZeneli <likzeneli123@gmail.com>
2024-01-30 15:00:29 -0800

    mesatarja

---
+++ src/Parametra2.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Parametra2 {
+
+    public static void main(String[] args) {
+        mesatarja();
+
+    }
+
+    public static double mesatarja(){
+        double mesatarja=1.9;
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+
+        }
+        return mesatarja;
+    }
+}
