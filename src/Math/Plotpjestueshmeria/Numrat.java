1f27cbdad154fba8f50966c1cf505dc1bc5c1bae
KastriotBlakaj <lotiblakaj123@gmail.com>
2024-02-21 21:41:29 +0100

    homework

---
+++ src/Math/Plotpjestueshmeria/Numrat.java
@@ -0,0 +1,9 @@
+package src.Math.Plotpjestueshmeria;
+
+public class Numrat {
+    public static void main(String[] args) {
+        // Itero ne gjateisne e Stringut dhe gjej mese gjateia e stringut eshte numer qift apo tek
+        String emri = "An sincerity so extremity he additions. Her yet there truth merit";
+
+    }
+}
