a9b030bccbd051769e234320fd56207b4ec97e09
LikZeneli <likzeneli123@gmail.com>
2024-02-23 09:12:49 -0800

    perseritja

---
+++ src/Perseritja/Perseritje.java
@@ -0,0 +1,42 @@
+package src.Perseritja;
+
+public class Perseritje {
+
+    public static int mosha;
+    public static String emri;
+    public static double gjatesia;
+    public static boolean kerri;
+
+    public static String mosha_1="8";
+    public static int gjatesia_2=1;
+    public static double gjersia=1.83;
+    public static boolean biciklla=true;
+
+    public static int metoda1(){
+        return 5;
+    }
+    public static String metoda2(){
+        return "t";
+    }
+    public static double metoda3(){
+        return 1.89;
+    }
+    public static boolean metoda4(){
+        return true;
+    }
+    public static void motorri(){
+        int []array={2,3,4,5,55,6,78,87,8,4};
+        double[]array1={2.33,3.45,4.44,5.34,6.44};
+        String[]arrays={"12","34","43","51","43"};
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
